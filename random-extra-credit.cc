2d0a8690fce0ffae86f29387d4266b9c35e240b2
Hannah Wynne <46692215+hwyn12@users.noreply.github.com>
2019-03-26 23:04:33 -0400

    Done! @nasseef

--- random-extra-credit.cc
+++ random-extra-credit.cc
@@ -52,18 +52,3 @@ int search(int random[], int size, int target){
         }
     } return -1;
 }
-
-
-/* Extra credit: Due Tuesday 3/26/2019 (11:59 PM)
- * Write a program that creates 20 random integers and stores them into
- * an array of integers.
- * Write the sequential search function.
- * In your main program, output all the elements of the array. Ask the user for
- * a target number and call the search function to find and locations of the target
- * in the array. Output the location.
- *
- * Create a public repository on GitHub, push your code to the repository and
- * tag me (@nasseef) in the final commit message. Please make sure your program works
- * before you tag me.
- * Do not email me your program.
- */
